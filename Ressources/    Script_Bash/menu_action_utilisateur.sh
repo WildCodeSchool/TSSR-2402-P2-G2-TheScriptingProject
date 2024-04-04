dd7d5e53e145a699082beae73c7a51c3df14fb92
Mr-Maglor <159529274+Mr-Maglor@users.noreply.github.com>
2024-04-04 13:53:37 +0200

    Update menu_action_utilisateur.sh

--- Ressources/    Script_Bash/menu_action_utilisateur.sh
+++ Ressources/    Script_Bash/menu_action_utilisateur.sh
@@ -12,6 +12,9 @@ afficher_menu() {
     echo '        Menu Action utilisateur           '
 
     echo "=================================================="
+    echo ""
+    echo "Machine distante : $nom_distant@$ip_distante"
+    echo ""
     echo "[1] Création de compte utilisateur local"
     echo "[2] Changement de mot de passe"
     echo "[3] Suppression de compte utilisateur local"
@@ -181,12 +184,13 @@ supprimer_utilisateur_groupe()
 # Demande d'infos sur la machine distante
     echo "=================================================="
     echo "        Initialisation script pour connexion      "
-    echo "=================================================="
+   	echo "=================================================="
 	echo ""
-	read -p "Veuillez entrer le nom de la machine distante : " nom_distant
+# Demande du nom d'utilisateur de la machine distante
+	read -p "Veuillez entrer le nom d'utilisateur de la machine distante : " nom_distant
+# Demande de l'adresse IP de la machine distante
 	read -p "Veuillez entrer l'adresse IP de la machine distante : " ip_distante
-
-
+	echo ""
 
 #  Boucle principale pour afficher le menu et traiter les options
 while true; do
