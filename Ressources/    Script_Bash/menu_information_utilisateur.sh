1ac72acef0fce7bbb732beed3707357fe4715136
Mr-Maglor <159529274+Mr-Maglor@users.noreply.github.com>
2024-04-04 13:43:37 +0200

    Update menu_information_utilisateur.sh
    mise en forme

--- Ressources/    Script_Bash/menu_information_utilisateur.sh
+++ Ressources/    Script_Bash/menu_information_utilisateur.sh
@@ -22,128 +22,126 @@ afficher_menu() {
     echo ""
 }
 
-# Fonctions Informations
-
-info_connexion() {
-
-    # Demande quel utilisateur
+# Fonctions Informations utilsiateur
+info_connexion() 
+{
+# Demande quel utilisateur
     echo ""
     echo "Date de dernière connexion"
     read -p "Tapez le nom d'utilisateur souhaité : " user_inf
 
-    # Est-ce que le nom existe sur le systeme ?
+# Est-ce que le nom existe sur le systeme ?
     if ssh $nom_distant@$ip_distante cat /etc/passwd | grep $user_inf >/dev/null; then
-        # si oui -> affichage dernière connexion
+     # si oui -> affichage dernière connexion
         ssh $nom_distant@$ip_distante lastlog -u $user_inf && sleep 2s
         echo "Dernière connexion : " >>/home/wilder/Documents/info-$user_inf-$(date +%Y-%m-%d).txt
         ssh $nom_distant@$ip_distante lastlog -u $user_inf >>/home/wilder/Documents/info-$user_inf-$(date +%Y-%m-%d).txt
         echo ""
         echo "Les données sont enregistrées dans le fichier info-$user_inf-$(date +%Y-%m-%d).txt" && sleep 3s
     else
-        # si non -> sortie du script
+    # si non -> sortie du script
         echo "L'utilisateur $user_inf n'existe pas" && sleep 2s
     fi
 }
 
-info_modification() {
-
-    # Demande quel utilisateur
+# Fonctions Informations dernière modification mot de passe
+info_modification() 
+{
+ # Demande quel utilisateur
     echo ""
     echo "Date de dernière modification du mdp"
     read -p "Tapez le nom d'utilisateur souhaité : " user_inf
-
-    # Est-ce que le nom existe sur le systeme ?
+# Est-ce que le nom existe sur le systeme ?
     if $nom_distant@$ip_distante cat /etc/passwd | grep $user_inf >/dev/null; then
-        # si oui -> affichage dernière connexion
+    # si oui -> affichage dernière connexion
         ssh $nom_distant@$ip_distante chage -l $user_inf | head -n 1 && sleep 2s
         ssh $nom_distant@$ip_distante chage -l $user_inf | head -n 1 >>/home/wilder/Documents/info-$user_inf-$(date +%Y-%m-%d).txt
         echo ""
         echo "Les données sont enregistrées dans le fichier info-$user_inf-$(date +%Y-%m-%d).txt" && sleep 3s
     else
-        # si non -> sortie du script
+    # si non -> sortie du script
         echo "L'utilisateur $user_inf n'existe pas" && sleep 2s
     fi
 
 }
 
-liste_sessions() {
-    # Demande quel utilisateur
+# Fonctions Informations liste session ouvertes
+liste_sessions() 
+{
+# Demande quel utilisateur
     echo ""
     echo "Liste des sessions ouvertes pour l'utilisateur"
     echo ""
     read -p "Tapez le nom d'utilisateur souhaité : " user_inf
-
-    # Est-ce que le nom existe sur le systeme ?
+# Est-ce que le nom existe sur le systeme ?
     if $nom_distant@$ip_distante cat /etc/passwd | grep $user_inf >/dev/null; then
-        # si oui -> affichage des sessions
+    # si oui -> affichage des sessions
         ssh $nom_distant@$ip_distante w -u $user_inf && sleep 2s
         ssh $nom_distant@$ip_distante w -u $user_inf >>/home/wilder/Documents/info-$user_inf-$(date +%Y-%m-%d).txt
         echo ""
         echo "Les données sont enregistrées dans le fichier info-$user_inf-$(date +%Y-%m-%d).txt" && sleep 3s
-
     else
-        # si non -> sortie du script
+    # si non -> sortie du script
         echo "L'utilisateur $user_inf n'existe pas" && sleep 2s
     fi
 
 }
 
-droits_dossier() {
-    # Demande quel utilisateur
+# Fonctions Informations droit dossier
+droits_dossier() 
+{
+# Demande quel utilisateur
     echo ""
     echo "Visualisation des droits sur un dossier"
     echo ""
     read -p "Tapez le nom d'utilisateur souhaité : " user_inf
-
-    # Est-ce que le nom existe sur le systeme ?
+# Est-ce que le nom existe sur le systeme ?
     if $nom_distant@$ip_distante cat /etc/passwd | grep $user_inf >/dev/null; then
-        # si oui -> demande quel dossier à verifier
-        read -p "Sur quel dossier souhaitez-vous vérifier les droits ?
-(spécifier chemin du dossier)" dossier_a
+    # si oui -> demande quel dossier à verifier
+        read -p "Sur quel dossier souhaitez-vous vérifier les droits ? (spécifier chemin du dossier)" dossier_a
         if $nom_distant@$ip_distante [ -d $dossier_a ]; then
-            # affichage des droits
+    # affichage des droits
             ssh $nom_distant@$ip_distante ls -ld $dossier_a/ && sleep 2s
             ssh $nom_distant@$ip_distante ls -ld $dossier_a/ >>/home/wilder/Documents/info-$user_inf-$(date +%Y-%m-%d).txt
             echo ""
             echo "Les données sont enregistrées dans le fichier info-$user_inf-$(date +%Y-%m-%d).txt" && sleep 3s
         else
-            # si non -> sortie du script
+        # si non -> sortie du script
             echo "Le dossier $dossier_a n'existe pas" && sleep 2s
         fi
     else
-        # si non -> sortie du script
+    # si non -> sortie du script
         echo "L'utilisateur $user_inf n'existe pas" && sleep 2s
     fi
 }
 
-droits_fichier() {
-
-    # Demande quel utilisateur
+# Fonctions Informations droit fichier
+droits_fichier() 
+{
+# Demande quel utilisateur
     echo ""
     echo "Visualisation des droits sur un fichier"
     echo ""
     read -p "Tapez le nom d'utilisateur souhaité : " user_inf
-
-    # Est-ce que le nom existe sur le systeme ?
+# Est-ce que le nom existe sur le systeme ?
     if $nom_distant@$ip_distante cat /etc/passwd | grep $user_inf >/dev/null; then
-        # si oui -> demande quel dossier à verifier
+    # si oui -> demande quel dossier à verifier
         read -p "Sur quel fichier souhaitez-vous vérifier les droits ?
 (spécifier chemin du fichier)" fichier_a
         if $nom_distant@$ip_distante [ -f $fichier_a ]; then
-            # affichage des droits
+        # affichage des droits
             ssh $nom_distant@$ip_distante ls -l $fichier_a && sleep 2s
             ssh $nom_distant@$ip_distante ls -l $fichier_a >>/home/wilder/Documents/info-$user_inf-$(date +%Y-%m-%d).txt
             echo ""
             echo "Les données sont enregistrées dans le fichier info-$user_inf-$(date +%Y-%m-%d).txt" && sleep 3s
         else
-            # si non -> sortie du script
+        # si non -> sortie du script
             echo "Le fichier $fichier_a n'existe pas" && sleep 2s
         fi
     else
-        # si non -> sortie du script
+    # si non -> sortie du script
         echo "L'utilisateur $user_inf n'existe pas" && sleep 2s
     fi
-
 }
 
 # Boucle principale pour afficher le menu et traiter les options
@@ -180,4 +178,3 @@ while true; do
         ;;
     esac
 done
-
