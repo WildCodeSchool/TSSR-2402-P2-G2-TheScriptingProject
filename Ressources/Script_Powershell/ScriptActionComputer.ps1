6cf17de66a485a971c9758ace6664f4270ddc44f
Mr-Maglor <159529274+Mr-Maglor@users.noreply.github.com>
2024-04-10 03:51:56 +0200

    Update ScriptActionComputer.ps1
    ajout fonction règle pare-feu

--- Ressources/Script_Powershell/ScriptActionComputer.ps1
+++ Ressources/Script_Powershell/ScriptActionComputer.ps1
@@ -221,6 +221,111 @@ function FirewallOff
 	}
 }
 
+
+# Fonction "Règles du pare-feu"
+function FirewellRules
+ {
+
+    # Demande de confirmation + Avertissement concernant la sortie du script dès l'éxécution de cette fonction
+    Write-Host "ATTENTION : Les commandes suivantes sont réservées à un public averti"
+    $ConfFwRules = Read-Host "Confirmez-vous l'accès à la modification des règles du pare-feu ? [O Pour valider] : "
+
+    # Si confirmation OK, affichage du sous-menu de la fonction "Règles du pare-feu"
+    if ($ConfFwRules -eq "O") {
+        Write-Host " [1] Affichage de l'état actuel des règles du pare-feu"
+        Write-Host " [2] Création d'une règle pour ouvrir port TCP"
+        Write-Host " [3] Création d'une règle pour ouvrir port UDP"
+        Write-Host " [4] Suppression d'une règle"
+        Write-Host " [5] Réinitialiser le pare-feu"
+        Write-Host " [*] Revenir au menu précédent"
+
+        while ($true) {
+            $ConfMessageFw = Read-Host "Faites votre choix parmi la sélection ci-dessus "
+            switch ($ConfMessageFw) {
+                # Affichage de l'état actuel du pare-feu
+                1 {
+                    Write-Host "Affichage de l'état actuel des règles du pare-feu"
+                    Invoke-Command -ComputerName $IpDistante -ScriptBlock { Get-NetFirewallRule } -Credential wilder
+                    Start-Sleep -Seconds 2
+					$Continuer = Read-Host "Appuyez sur une touche pour contiuer"
+                }
+                # Exécution de la commande d'ouverture de port TCP
+                2 {
+                    Write-Host "Ouverture d'un port TCP sur tout les profiles"
+                    Start-Sleep -Seconds 2
+                    $OpenTCP = Read-Host "Indiquer le n° du port à ouvrir "
+					$NomdeRegleTCP = Read-Host "Spécifier le nom de la règle "
+					$commandfw= {
+					param($DisplayName)	
+					New-NetFirewallRule -DisplayName $DisplayName -Direction inbound -Profile Any -Action Allow -LocalPort $OpenTCP -Protocol TCP
+					}
+	                Write-Host "Ouverture du port TCP $OpenTCP"
+                    Start-Sleep -Seconds 2
+                    Invoke-Command -ComputerName $IpDistante -ScriptBlock $commandfw -ArgumentList $NomdeRegleTCP -Credential wilder
+                    Write-Host "Port TCP $OpenTCP ouvert"
+                    Start-Sleep -Seconds 2
+                }
+			  	# Exécution de la commande d'ouverture de port UDP
+				3 {
+                    Write-Host "Ouverture d'un port UDP sur tout les profiles"
+                    Start-Sleep -Seconds 2
+                    $OpenUDP = Read-Host "Indiquer le n° du port à ouvrir "
+					$NomdeRegleUDP = Read-Host "Spécifier le nom de la règle "
+					$commandfw= {
+					param($DisplayName)	
+					New-NetFirewallRule -DisplayName $DisplayName -Direction inbound -Profile Any -Action Allow -LocalPort $OpenUDP -Protocol UDP
+					}
+	                Write-Host "Ouverture du port UDP $OpenUDP"
+                    Start-Sleep -Seconds 2
+                    Invoke-Command -ComputerName $IpDistante -ScriptBlock $commandfw -ArgumentList $NomdeRegleUDP -Credential wilder
+                    Write-Host "Port UDP $OpenUDP ouvert"
+                    Start-Sleep -Seconds 2
+                }
+				# Exécution de la commande de fermeture de port
+                4 {
+                    Write-Host "Suppression d'une règle"
+                    Start-Sleep -Seconds 2
+                    $RegleSuppr = Read-Host "Indiquer la règle à supprimer : "
+					$commandfw = {
+					param($RulesNames)
+					Remove-NetFirewallRule -displayName $RulesNames}
+                    Start-Sleep -Seconds 2
+                    Invoke-Command -ComputerName $IpDistante -ScriptBlock $commandfw -ArgumentList $RegleSuppr -Credential wilder
+                    Write-Host "$RegleSuppr règle supprimé"
+                    Start-Sleep -Seconds 2
+                }	
+                # Exécution de la commande de réinitialisation du pare-feu + Avertissement
+                5 {
+                    Write-Host "Réinitialisation du pare-feu"
+                    Write-Host "ATTENTION, cette commande peut compromettre la connexion à distance"
+                    $ConfResetFw = Read-Host "Souhaitez-vous tout de même continuer ? [O pour valider] : "
+                    # Si confirmation OK, exécution de la commande de réinitialisation du pare-feu
+                    if ($ConfResetFw -eq "O") {
+                        Start-Sleep -Seconds 1
+                        Invoke-Command -ComputerName $IpDistante -ScriptBlock { netsh advfirewall reset }  -Credential wilder
+                        Write-Host "Le pare-feu a été réinitialisé"
+                        Start-Sleep -Seconds 2
+                        return
+                    }
+                    # Si confirmation NOK, sortie de la fonction "Règles du pare-feu"
+                    else {
+                        Write-Host "Opération annulée - Retour au menu précédent"
+                        Start-Sleep -Seconds 1
+                    }
+                }
+                # Si autre/mauvais choix, sortie de la fonction "Règles du pare-feu"
+                default {
+                    Write-Host "Mauvais choix - Retour au menu précédent"
+                    Start-Sleep -Seconds 1
+                    return
+                }
+            }
+        }
+    }
+}
+
+
+
 $NomDistant = Read-Host "Quel est le nom de l'utilisateur/poste distant"
 $IpDistante = Read-Host "Quelle est l'adresse IP du poste distant"
 Start-Sleep -Seconds 1
