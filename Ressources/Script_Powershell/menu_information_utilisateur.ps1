73ffee9cd1fc74ccf89f8941eda0cd6ef5297518
Seyia11 <159759653+Seyia11@users.noreply.github.com>
2024-04-17 09:40:49 +0200

    Update menu_information_utilisateur.ps1

--- Ressources/Script_Powershell/menu_information_utilisateur.ps1
+++ Ressources/Script_Powershell/menu_information_utilisateur.ps1
@@ -28,41 +28,4 @@ function InfoConnexion
     }
 }
 
-function ChangerMotDePasse 
-{
-    # Demande changement de mot de passe -> pour quel utilisateur?
-    $userMdp = Read-Host "Pour quel compte utilisateur souhaitez-vous modifier le mot de passe?"
 
-    # Vérification si l'utilisateur existe
-    $userExists = ssh "$nom_distant@$ip_distante" "net user $userMdp" 2>$null
-    if ($userExists) {
-        # Si l'utilisateur existe, demander le nouveau mot de passe
-        $newPassword = Read-Host "Entrez le nouveau mot de passe pour $userMdp" -AsSecureString
-        $confirmPassword = Read-Host "Confirmez le nouveau mot de passe pour $userMdp" -AsSecureString
-
-        # Convertir les SecureStrings en plain text pour pouvoir comparer 
-        $newPasswordPlain = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($newPassword))
-        $confirmPasswordPlain = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($confirmPassword))
-
-        # Vérifier si les mots de passe correspondent
-        if ($newPasswordPlain -eq $confirmPasswordPlain) {
-            # Changer le mot de passe de l'utilisateur
-            Invoke-Command -ComputerName $ip_distante -Credential wilder -ScriptBlock {
-                param($user, $password)
-                net user $user $password
-            } -ArgumentList $userMdp, $newPasswordPlain
-
-            Write-Host "Le mot de passe de l'utilisateur $userMdp a été modifié avec succès." -ForegroundColor Green
-            Start-Sleep -Seconds 2
-        }
-        else {
-            Write-Host "Les mots de passe ne correspondent pas. Veuillez réessayer." -ForegroundColor Red
-            Start-Sleep -Seconds 2
-        }
-    }
-    else {
-        # Si l'utilisateur n'existe pas
-        Write-Host "L'utilisateur $userMdp n'existe pas sur le poste distant." -ForegroundColor Red
-        Start-Sleep -Seconds 2
-    }
-}
